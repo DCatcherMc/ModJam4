999d1d49dfc11da3e4b82eb2daf1db3352b93e6a
DCatcher <daniel.catchpole@live.com>
2014-05-15 14:15:47 +0100

    Commit

--- src/main/java/net/dcatcher/modjam4/common/player/PlayerTracker.java
+++ src/main/java/net/dcatcher/modjam4/common/player/PlayerTracker.java
@@ -1,10 +1,14 @@
 package net.dcatcher.modjam4.common.player;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.dcatcher.modjam4.ModJam4;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EntityTracker;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
@@ -20,10 +24,30 @@ public class PlayerTracker {
         }
     }
 
+    @SubscribeEvent
     public void onPlayerDeath(LivingDeathEvent event){
         if(!event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer){
             NBTTagCompound playerData = new NBTTagCompound();
             DCPlayerProperties.getProps(event.entity).saveNBTData(playerData);
         }
+    }
+
+    @SubscribeEvent
+    public void onLivingEntityJoinWorld(EntityJoinWorldEvent event){
+        Entity entity = event.entity;
+
+        if(entity instanceof EntityPlayer){
+            EntityPlayer player = (EntityPlayer)entity;
+            NBTTagCompound playerData = ModJam4.proxy.getLevels(player.getDisplayName());
+            if(playerData != null){
+                DCPlayerProperties.getProps(player).loadNBTData(playerData);
+                DCPlayerProperties.getProps(player).sync();
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerLogin(cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event){
+        DCPlayerProperties.getProps(event.player).sync();
     }
 }
