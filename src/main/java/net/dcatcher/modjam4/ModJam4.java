46423ade7ea79d09aeaa2fd00c21cb07edeaecbf
DCatcher <daniel.catchpole@live.com>
2014-05-15 10:32:51 +0100

    Commit

--- src/main/java/net/dcatcher/modjam4/ModJam4.java
+++ src/main/java/net/dcatcher/modjam4/ModJam4.java
@@ -4,9 +4,13 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import net.dcatcher.modjam4.common.items.ItemHandler;
 import net.dcatcher.modjam4.common.util.DCCreativeTab;
 import net.minecraft.creativetab.CreativeTabs;
+
+import java.net.NetworkInterface;
+import java.nio.channels.NetworkChannel;
 
 /**
  * TODO:
@@ -14,6 +18,7 @@ import net.minecraft.creativetab.CreativeTabs;
  */
 
 @Mod(modid="DCatcher_ModJam4", name="To be decided!", version="${version}")
+
 public class ModJam4 {
 
     public static CreativeTabs danTab = new DCCreativeTab();
@@ -25,8 +30,7 @@ public class ModJam4 {
 
     @Mod.EventHandler
     public static void Init(FMLInitializationEvent event){
-
-
+        NetworkRegistry.INSTANCE.newChannel()
     }
 
     @Mod.EventHandler
---
+++ src/main/java/net/dcatcher/modjam4/common/network/DCChannelHandler.java
@@ -0,0 +1,25 @@
+package net.dcatcher.modjam4.common.network;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Copyright: DCatcher
+ */
+
+public class DCChannelHandler implements ChannelHandler{
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+}
