008c899aae6a25c53f4c15da77f6700d8c8545da
DCatcher <daniel.catchpole@live.com>
2014-05-15 13:48:57 +0100

    Commit

--- src/main/java/net/dcatcher/modjam4/common/player/PlayerTracker.java
+++ src/main/java/net/dcatcher/modjam4/common/player/PlayerTracker.java
@@ -1,7 +1,27 @@
 package net.dcatcher.modjam4.common.player;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.EntityTracker;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
 /**
  * Copyright: DCatcher
  */
-public class PlayerTracker implements II {
+public class PlayerTracker {
+
+    @SubscribeEvent
+    public void onPlayerLogin(PlayerEvent.EntityConstructing event){
+        if(event.entity instanceof EntityPlayer && DCPlayerProperties.getProps(event.entity) != null){
+            DCPlayerProperties.register((EntityPlayer)event.entity);
+        }
+    }
+
+    public void onPlayerDeath(LivingDeathEvent event){
+        if(!event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer){
+            
+        }
+    }
 }
