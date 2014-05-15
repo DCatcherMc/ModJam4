bca0abd9c6b3401e7bcd73afa8e45cc6a6e453ba
DCatcher <daniel.catchpole@live.com>
2014-05-15 08:46:03 +0100

    Initial Commit to start the JAM!

---
+++ src/main/java/net/dcatcher/modjam4/common/ModJam4.java
@@ -0,0 +1,30 @@
+package net.dcatcher.modjam4.common;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * TODO:
+ *  Name this main mod something different.
+ */
+
+@Mod(modid="DCatcher_ModJam4", name="To be decided!", version="modjamDev")
+public class ModJam4 {
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public static void Init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event){
+
+    }
+}
