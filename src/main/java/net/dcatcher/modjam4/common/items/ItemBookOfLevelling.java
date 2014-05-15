b5c1e54dccc2d511fe935c3c8e41060e7c8f6880
DCatcher <daniel.catchpole@live.com>
2014-05-15 10:03:15 +0100

    Commit

--- src/main/java/net/dcatcher/modjam4/common/items/ItemBookOfLevelling.java
+++ src/main/java/net/dcatcher/modjam4/common/items/ItemBookOfLevelling.java
@@ -1,6 +1,8 @@
 package net.dcatcher.modjam4.common.items;
 
 import net.dcatcher.modjam4.ModJam4;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
 
 /**
  * Copyright: DCatcher
@@ -10,5 +12,10 @@ public class ItemBookOfLevelling extends net.minecraft.item.Item {
     public ItemBookOfLevelling(){
         setCreativeTab(ModJam4.danTab);
         setUnlocalizedName("itemBookOfLevelling");
+    }
+
+    @Override
+    public void registerIcons(IIconRegister icons) {
+        this.itemIcon = icons.registerIcon("dcatcher_modjam:bookOfLeveling");
     }
 }
