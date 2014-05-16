890578f8fd395e6643852b767c1bc3ae33ff9492
DCatcher <daniel.catchpole@live.com>
2014-05-16 14:01:31 +0100

    Commit

--- src/main/java/net/dcatcher/enderius/common/items/ItemEnderFocus.java
+++ src/main/java/net/dcatcher/enderius/common/items/ItemEnderFocus.java
@@ -4,6 +4,7 @@ import net.dcatcher.enderius.Enderius;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -25,16 +26,31 @@ public class ItemEnderFocus extends Item {
     }
 
     @Override
-    public boolean onItemUse(ItemStack stack, EntityPlayer entityPlayer, World world, int par4,
-                             int par5, int par6, int par7, float par8, float par9, float par10) {
-        Block clicked = world.getBlock(par4, par5, par6);
+    public boolean onItemUse(ItemStack stack, EntityPlayer entityPlayer, World world, int x,
+                             int y, int z, int side, float px, float py, float pz) {
+        removeBlocks(world, x, y, z, side);
+        return true;
+    }
 
-        for(int i = 1; i < 4; i++){
-            for(int j = 1; j < 4; j++){
-
-            }
+    public void removeBlocks(World world, int x, int y, int z, int side){
+        switch(side){
+            case 0 || 1:
+                int startXa = x - 1;
+                int startZa = z + 1;
+                for(int a = 0; a < 3; x++){
+                    for(int b = 3; b < 3; b++){
+                        world.setBlockToAir(startXa + a, y, startZa + b);
+                    }
+                }
+                break;
+            case 2 | 3:
+                int startXb = x - 1;
+                int startYb = y + 1;
+                break;
+            case 4 | 5:
+                int startZc = z - 1;
+                int startYc = y + 1;
+                break;
         }
-
-        return false;
     }
 }
