57b2b924e2c019f23862aabb6f10c35f3fe93c26
DCatcher <daniel.catchpole@live.com>
2014-05-17 14:14:23 +0100

    Commit

--- src/main/java/net/dcatcher/enderius/client/gui/GuiRepellent.java
+++ src/main/java/net/dcatcher/enderius/client/gui/GuiRepellent.java
@@ -3,6 +3,8 @@ package net.dcatcher.enderius.client.gui;
 import cpw.mods.fml.client.GuiConfirmation;
 import net.dcatcher.enderius.common.tileentities.ContainerRepellent;
 import net.dcatcher.enderius.common.tileentities.TileEntityRepellent;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.texture.ITextureObject;
 import net.minecraft.inventory.Container;
@@ -14,6 +16,9 @@ import net.minecraft.util.ResourceLocation;
 public class GuiRepellent extends GuiContainer {
 
     TileEntityRepellent tileEntity;
+
+    private GuiTextField xCoord, yCoord, zCoord;
+
 
     public GuiRepellent(TileEntityRepellent te) {
         super(new ContainerRepellent(te));
@@ -32,5 +37,25 @@ public class GuiRepellent extends GuiContainer {
     @Override
     protected void drawGuiContainerForegroundLayer(int p1, int p2) {
         fontRendererObj.drawString("Position to teleport users out of: ", 5, 5, 4210752);
+
+        xCoord = new GuiTextField(fontRendererObj, 5, 10, 40, 10);
+        xCoord.drawTextBox();
+        yCoord = new GuiTextField(fontRendererObj, 5, 20, 40, 10);
+        yCoord.drawTextBox();
+
+        zCoord = new GuiTextField(fontRendererObj, 5, 30, 40, 10);
+        zCoord.drawTextBox();
+
+
+
+    }
+
+    @Override
+    protected void mouseClicked(int i, int j, int k) {
+        super.mouseClicked(i, j, k);
+    }
+
+    public void syncToTILEENTITY(GuiButton butt){
+        //tileEntity.locX =
     }
 }
